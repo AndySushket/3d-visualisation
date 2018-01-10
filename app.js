22fb570bbc87e88072ec91a32e1157cd1731bb37
Andy Sushket <ellyson00@gmail.com>
2018-01-10 23:54:58 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ class Water {
 
 	constructor() {
 		this.audio = new Audio();
-		this.audio.src = "music3.mp3"
+		this.audio.src = "music5.flac";
 
 		this.renderer = new THREE.WebGLRenderer({antialias: true});
 		this.renderer.shadowMap.enabled = true;
@@ -48,8 +48,8 @@ class Water {
 	}
 	initControls(){
 		// this.controls = new THREE.OrbitControls( this.camera );
-		this.camera.position.set(235.9, 68.8, 226);
-		this.camera.rotation.set(-0.512, 0.8919, 0.41258);
+		this.camera.position.set(171.6, 68.7, 177.1);
+		this.camera.rotation.set(-0.473, 0.625, 0.291);
 		// this.controls.update();
 
 	}
@@ -61,22 +61,27 @@ class Water {
 		this.analyser = audioCtx.createAnalyser();
 		let bufferLength = this.analyser.frequencyBinCount;
 		this.analyser.fftSize = 1024;
+		this.analyser.smoothingTimeConstant = 0.90;
 		audioSrc.connect(this.analyser);
 
 		this.dataArray = new Uint8Array(bufferLength);
+		this.timeByteData = new Uint8Array(bufferLength)
 		this.analyser.connect(audioCtx.destination);
 
 	}
 	initCubes(){
 		let color = new THREE.Color();
 		var x = 0, z = 0;
-		for(let i = 0;i < this.dataArray.length - 11; i++){
+		for(let i = 0;i < this.analyser.frequencyBinCount; i++){
 
 			let geometry = new THREE.CubeGeometry(2,2,2);
 			let material = new THREE.MeshLambertMaterial({
 				color: color
 			});
 			let mesh = new THREE.Mesh(geometry, material);
+			mesh.material.color.r *= 0.1;
+			mesh.material.color.b = 1 - 1 / 50;
+			mesh.material.color.r *= mesh.scale.y;
 			mesh.userData = color;
 			mesh.position.set(x, 0, z);
 			this.scene.add(mesh);
@@ -92,16 +97,29 @@ class Water {
 
 	animate(){
 		this.scene.traverse((e) => {
+			let data = this.dataArray;
+			this.analyser.getByteFrequencyData(this.dataArray);
+			this.analyser.getByteTimeDomainData(this.timeByteData);
 			if(e instanceof THREE.Mesh && e.geometry.type !== "PlaneGeometry"){
-				new TWEEN.Tween(e.scale)
-					.to({x: 1,y: (this.dataArray[e.id - 1] * 1.5 - 250 > 0.1) ? this.dataArray[e.id - 1] * 1.5 - 250 : 0.1, z: 1}, 100)
-					.onUpdate(function() {
-						e.material.color.r = e.userData.r * e.scale.y / 20;
-					}).start();
+
+				if (data[e.id-1] !== 0) {
+					let zscale = 1 + data[e.id-1] * 0.1;
+					e.scale.y = zscale ;
+					e.material.color.r = e.userData.r * e.scale.y / 15;
+					e.material.color.b = 1-e.userData.r * e.scale.y / 50;
+				}
+				// new TWEEN.Tween(e.scale)
+				// 	.to({}, 100)
+				// 	.onUpdate(function() {
+				// 		e.material.color.r = e.userData.r * e.scale.y / 20;
+				// 	}).start();
+				// e.scale.set(1,(this.dataArray[e.id - 1] * 1.5 - 250 > 0.1) ? this.dataArray[e.id - 1] * 1.5 - 250 : 0.1,  1);
 			}
 		});
-		TWEEN.update();
-		this.analyser.getByteTimeDomainData(this.dataArray);
+		// TWEEN.update();
+		// this.analyser.getByteTimeDomainData(this.dataArray);
+		// console.log(this.camera);
+
 		this.renderer.render(this.scene,this.camera);
 		requestAnimationFrame(this.animate.bind(this));
 	}
