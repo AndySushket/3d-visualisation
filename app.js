b1d91497d49a1d8c09a89c4ac9c9da398e2e7c41
Andy Sushket <ellyson00@gmail.com>
2018-01-14 00:46:26 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 /**
  * Created by Ellyson on 10/1/2017.
  */
-class Water {
+class Visualisation {
 
 	constructor() {
 		this.audio = new Audio();
 		this.audio.src = "music4.mp3";
 
-		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer = new THREE.WebGLRenderer({antialias: true,alpha: true});
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.sortObjects = false
 		this.renderer.shadowMap.enabled = true;
 		this.renderer.setClearColor(0xcbc3b8);
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		// this.renderer.setSize(window.innerWidth, window.innerHeight);
 		document.getElementById("webgl-container").appendChild(this.renderer.domElement);
 
 		window.addEventListener("resize", this.resize.bind(this));
@@ -29,9 +32,11 @@ class Water {
 		this.light = new THREE.DirectionalLight(new THREE.Color("#0xffffff"));
 		this.light.position.set(170, 150, 100);
 		this.scene.add(this.light);
+		let texture = new THREE.TextureLoader().load("intothree.png");
+		this.scene.background = texture;
 
 		this.initControls();
-		this.initGround();
+		// this.initGround();
 		this.initAudioObject();
 		this.audio.play();
 		this.initCubes();
@@ -39,17 +44,17 @@ class Water {
 	}
 	initGround(){
 
-		var geometry = new THREE.PlaneGeometry(320, 320, 32 );
-		var material = new THREE.MeshBasicMaterial( {color: 0xcbc3b8, side: THREE.DoubleSide} );
-		var plane = new THREE.Mesh( geometry, material );
+		let geometry = new THREE.PlaneGeometry(1000, 1000, 32 );
+		let material = new THREE.MeshBasicMaterial( {color: 0xcbc3b8, side: THREE.DoubleSide} );
+		let plane = new THREE.Mesh( geometry, material );
 		plane.rotation.x = Math.PI/2;
 		plane.position.set(70, 0, 70);
 		this.scene.add( plane );
 	}
 	initControls(){
 		// this.controls = new THREE.OrbitControls( this.camera );
-		this.camera.position.set(171.6, 68.7, 177.1);
-		this.camera.rotation.set(-0.473, 0.625, 0.291);
+		this.camera.position.set(156.26, 61.33, 148.04);
+		this.camera.rotation.set(-0.48, 0.701, 0.324);
 		// this.controls.update();
 
 	}
@@ -60,36 +65,49 @@ class Water {
 		let audioSrc = audioCtx.createMediaElementSource(this.audio);
 		this.analyser = audioCtx.createAnalyser();
 		let bufferLength = this.analyser.frequencyBinCount;
-		this.analyser.fftSize = 1024;
+		this.analyser.fftSize = this.analyser.frequencyBinCount;
 		this.analyser.smoothingTimeConstant = 0.90;
 		audioSrc.connect(this.analyser);
 
 		this.dataArray = new Uint8Array(bufferLength);
-		this.timeByteData = new Uint8Array(bufferLength)
+		this.timeByteData = new Uint8Array(bufferLength);
 		this.analyser.connect(audioCtx.destination);
 
 	}
 	initCubes(){
 		let color = new THREE.Color();
-		var x = 0, z = 0;
-		for(let i = 0;i < this.analyser.frequencyBinCount; i++){
+		let x = 0, z = 0;
+		let deg = Math.PI/this.analyser.frequencyBinCount;
+		for(let i = 0;i < this.analyser.frequencyBinCount-2; i++){
 
 			let geometry = new THREE.CubeGeometry(2,2,2);
 			let material = new THREE.MeshLambertMaterial({
 				color: color
 			});
 			let mesh = new THREE.Mesh(geometry, material);
-			mesh.material.color.r *= 0.1;
-			mesh.material.color.b = 1 - 1 / 50;
-			mesh.material.color.r *= mesh.scale.y;
+			mesh.scale.y = .5;
+
+			mesh.material.color.g *= .5 ;
+			mesh.material.color.b = 1 ;
+			mesh.material.color.r = 0;
+
 			mesh.userData = color;
-			mesh.position.set(x, 0, z);
+
+			// mesh.position.set(100*Math.sin(2*deg*i), 0, 100*Math.cos(2*deg*i));
+			// mesh.rotation.y = 2*deg*i;
+			// mesh.rotation.z = Math.PI/2;
+
+			mesh.position.set(x,.5,z);
 			this.scene.add(mesh);
-			x += 5;
-			if(x >= 130){
+
+			x += 3;
+
+			if(x >= 90){
 				z += 5;
 				x = 0
 			}
+
+
 
 		}
 
@@ -98,16 +116,20 @@ class Water {
 	animate(){
 		this.scene.traverse((e) => {
 			let data = this.dataArray;
+
 			this.analyser.getByteFrequencyData(this.dataArray);
 			this.analyser.getByteTimeDomainData(this.timeByteData);
+
 			if(e instanceof THREE.Mesh && e.geometry.type !== "PlaneGeometry"){
 
 				if (data[e.id-1] !== 0) {
 					let zscale = 1 + data[e.id-1] * 0.1;
-					e.scale.y = zscale ;
-					e.material.color.r = e.userData.r * e.scale.y / 15;
-					e.material.color.b = 1-e.userData.r * e.scale.y / 50;
+					e.scale.y = zscale/2 ;
+					e.position.y = zscale/2;
+					e.material.color.g = e.userData.r * e.scale.y*.13 +0.4	;
+					// e.material.color.b = 1-e.userData.b * e.scale.y / 30;
 				}
+
 				// new TWEEN.Tween(e.scale)
 				// 	.to({}, 100)
 				// 	.onUpdate(function() {
@@ -116,6 +138,8 @@ class Water {
 				// e.scale.set(1,(this.dataArray[e.id - 1] * 1.5 - 250 > 0.1) ? this.dataArray[e.id - 1] * 1.5 - 250 : 0.1,  1);
 			}
 		});
+
+		// this.light.position.y = this.dataArray[50];
 		// TWEEN.update();
 		// this.analyser.getByteTimeDomainData(this.dataArray);
 		// console.log(this.camera);
@@ -124,4 +148,4 @@ class Water {
 		requestAnimationFrame(this.animate.bind(this));
 	}
 }
-let visualisation = new Water();
+let visualisation = new Visualisation();
